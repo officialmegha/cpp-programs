279c1a7c8b3c28f155dfc8996956caaba290254c
Megha Bhosale <megha.bhosale1503@gmail.com>
2021-03-13 17:06:51 +0530

    added oops programs

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,47 @@
-sdsadasdasdsdasdasdasda
-added some new stuff
-asdasdasdasdasd
+#include <iostream>
+using namespace std;
+
+class Student{
+    public:
+    int roll_no;
+    string name;
+    int age;
+
+    Student(){
+        cout<<"constructor has been called"<<endl;
+    }
+
+    Student(int no,string name1,int age1){
+        roll_no = no;
+        name = name1;
+        age = age1;
+    }
+
+    ~ Student(){
+        cout<<"destructor has been called"<<endl;
+    }
+
+    // void registerStudent(int no,string name1,int age1){
+    //     roll_no = no;
+    //     name = name1;
+    //     age = age1;
+    // }
+
+    void diplay(){
+        cout<<name<<endl;
+        cout<<roll_no<<endl;
+        cout<<age<<endl;
+    }
+};
+
+int main(){
+    Student s;
+    Student s1(1,"test",20),s2(2,"tes1",20);
+    // s1.registerStudent(1,"test",20);
+    // s2.registerStudent(2,"test2",20);
+
+    s1.diplay();
+    s2.diplay();
+    
+    return 0;
+}
--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,56 @@
-sasdasd
+#include <iostream>
+using namespace std;
+
+class Student{
+    public:
+    static int roll_no;
+    string name;
+    int age;
+
+
+    Student(string name1,int age1){
+        name = name1;
+        age = age1;
+        roll_no++;
+        cout<<"\nStudent Registerd!\n";
+    }
+
+    void diplay(){
+        cout<<name<<endl;
+        cout<<roll_no<<endl;
+        cout<<age<<endl;
+    }
+};
+
+int Student::roll_no = 0;
+
+int main(){
+    cout<<"welcome to xyz school==================\n";
+    int p;
+    do{
+        cout<<"1. register student \n 2. exit\n=====>";
+        cin>>p;
+
+        switch(p){
+            case 1:
+            {
+                // register student
+                int age;
+                string name;
+                cout<<"enter name:";
+                cin>>name;
+                cout<<"enter age:";
+                cin>>age;
+                Student s1(name,age);
+                s1.diplay();
+                break;
+            }   
+            case 2:
+                p=2;
+                break;
+            default:
+                cout<<"enter right choice\n";    
+        }
+    }while(p!=2);
+    return 0;
+}
